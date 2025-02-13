d58187b390f4b35bd5839efdf3097b3c618135b9
bellalondon <144961213+bellalondon@users.noreply.github.com>
2025-02-13 14:22:15 -0500

    homepage

--- coursesearchplus/app/page.tsx
+++ coursesearchplus/app/page.tsx
@@ -10,17 +10,20 @@ export default function Home() {
       <NavbarHome />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-          <h1 className="text-4xl font-bold mb-4">
-            CourseSearchPlus
+          <h1 className="text-5xl font-bold mb-4 text-red-500">
+            Plan the right courses,
+            <span className="block text-right">
+              with the right professor.
+            </span>
           </h1>
-          <p className="text-xl mb-8">
-            Find and manage your courses easily
+          <p className="text-2xl mb-8 text-red-500">
+            Find and manage your courses easily with Course Search Plus!
           </p>
           <Link 
             href="/about" 
             className="text-foreground hover:underline"
           >
-            About Us →
+            Learn More →
           </Link>
         </div>
       </main>
