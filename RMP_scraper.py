ce8f19a982fda1dd21350d8e54860e9289bf7e49
Sophia Li <sophia1616li@gmail.com>
2025-03-25 14:23:58 -0400

    RMP code update

--- RMP_scraper.py
+++ RMP_scraper.py
@@ -20,6 +20,16 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
 }
 
+headersProf = {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "authorization": "Basic dGVzdDp0ZXN0",
+    "content-type": "application/json",
+    "origin": "https://www.ratemyprofessors.com",
+    "referer": osu_url,
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+}
+
 #GraphQL data query for professor information
 data = {
     "query": """query TeacherSearchPaginationQuery($count: Int!, $cursor: String, $query: TeacherSearchQuery!) {
@@ -36,6 +46,7 @@ data = {
                         department
                         avgRating
                         avgDifficulty
+                        id
                     }
                 }
                 pageInfo {
@@ -58,31 +69,32 @@ data = {
 
 def prof_ratings_query(prof_id):
     prof_data = {
-    "query": """query TeacherSearchPaginationQuery($count: Int!, $id: ID!, $courseFilter: String, $cursor: String, $query: TeacherSearchQuery!) {
+    "query": """query RatingsListQuery($count: Int!, $id: ID!, $courseFilter: String, $cursor: String) {
         node(id: $id) {
             ... on Teacher {
-                ratings(first: $count, after: $cursor) {
-                    edges {
-                        node {
-                            comment
-                            clarityRating
-                            difficultyRating
-                        }
-                    }
-                    pageInfo {
-                        hasNextPage
-                        endCursor
+            numRatings
+            ratings(first: $count, after: $cursor,courseFilter: $courseFilter) {
+                edges {
+                    node {
+                        comment
+                        clarityRating
+                        difficultyRating
                     }
                 }
+                pageInfo {
+                    hasNextPage
+                    endCursor
+                }
+            }
             }
         }
     }""",
     "variables": {
-        "count": 10,
+        "count": 30,
         "cursor": None,
         "id": prof_id
     }
-}
+    }
     return prof_data
 
 
@@ -95,36 +107,44 @@ es = Elasticsearch('https://localhost:9200', ssl_context=ctx, basic_auth=("elast
 
 #This method gets the reviews for each professor in rate my professor
 #using their ID, it gets a professor's review page and scrapes each review 
-def get_reviews(professor_id, j):
+def get_reviews(professor_id, j, id):
     # URL of the professor's page on RateMyProfessors
-    url = f'https://www.ratemyprofessors.com/professor/{professor_id}'
+    url = f"https://www.ratemyprofessors.com/professor/{professor_id}"
+    headersProf["referer"] = url
 
     # Initialize the Ratings list if it doesn't exist
     if "Ratings" not in j:
         j["Ratings"] = []
-        
+    
     index = 0
     # Load all the comments
     while True:
+        print("\nNew Page")
         # Get the current page comments
-        prof_data = prof_ratings_query(professor_id)
-        response = requests.post(graphql_url, headers=headers, json=prof_data).json()
-        
-        page_info = response["data"]["node"]["ratings"]["pageInfo"]
-        ratings = response["data"]["node"]["ratings"]["edges"]["node"]
+        prof_data = prof_ratings_query(id)
+        response = requests.post(graphql_url, headers=headersProf, json=prof_data).json()
 
+        ratings = response["data"]["node"]["ratings"]["edges"]
+        if response["data"]["node"]["numRatings"]==0:
+            return 
+        
         for rating in ratings:
-            j["Ratings"].append([index, rating["difficultyRating"],rating["comment"]])
+            j["Ratings"].append({
+                "index":index, 
+                "rating": rating["node"]["difficultyRating"],
+                "comment": rating["node"]["comment"]
+                })
             index += 1
+
+        page_info = response["data"]["node"]["ratings"]["pageInfo"]
 
         #pagination logic
         if not page_info["hasNextPage"]:
             break  # Stop if no more pages
 
         #update cursor for next request
-        data["variables"]["cursor"] = page_info["endCursor"]
-
-    #print(j["Ratings"])
+        prof_data["variables"]["cursor"] = page_info["endCursor"]
+    print(j)
         
 #This function uses the graphql endpoint and the given headers to get information about each professor
 #It returns when the professors being scraped either don't have any reviews or when there is no longer 
@@ -132,14 +152,13 @@ def get_reviews(professor_id, j):
 def search_school_professor():
     while True:
         #get html from graphql_url using set headers
-        response = requests.post(graphql_url, headers=headers, json=data).json()
-        
+        response = requests.post(graphql_url, headers=headers, json=data).json()        
         #for each professor, get their reviews and add elasticsearch index
         for professor in response["data"]["search"]["teachers"]["edges"]:            
             if professor["node"]["numRatings"]==0:
                 return 
-
-            get_reviews(professor["node"]["legacyId"],professor["node"])
+            print("\n" + professor["node"]["lastName"] + professor["node"]["firstName"])
+            get_reviews(professor["node"]["legacyId"],professor["node"],professor["node"]["id"])
             es.index(index='professors', id=professor["node"]["lastName"] + professor["node"]["firstName"], document=professor["node"])
 
         #pagination logic
